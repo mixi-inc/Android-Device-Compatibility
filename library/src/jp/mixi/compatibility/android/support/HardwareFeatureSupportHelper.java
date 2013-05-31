59397c850e37935eb5488ec869b3026d901a1505
hideyuki.kikuma <hideyuki.kikuma@mixi.co.jp>
2013-05-31 18:52:27 +0900

    Add hasFacingBackCamera
     − インカメラしかない場合Camera.open()がnullになってしまうので、事前確認したい場合に使用する。

--- library/src/jp/mixi/compatibility/android/support/HardwareFeatureSupportHelper.java
+++ library/src/jp/mixi/compatibility/android/support/HardwareFeatureSupportHelper.java
@@ -1,7 +1,11 @@
 package jp.mixi.compatibility.android.support;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.os.Build;
 
 import javax.inject.Inject;
 
@@ -30,5 +34,29 @@ public class HardwareFeatureSupportHelper {
      */
     public boolean hasCamera() {
         return mPackageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+    
+    /**
+     * Return has camera facing {@link CameraInfo.CAMERA_FACING_BACK}.
+     * 
+     *  Prevent {@link Camera.open} is null.
+     * 
+     * @return true if has facing back camera.
+     */
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    public boolean hasFacingBackCamera() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD) {
+            return true;
+        }
+
+        int numberOfCameras = Camera.getNumberOfCameras();
+        CameraInfo cameraInfo = new CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+                return true;
+            }
+        }
+        return false;
     }
 }
